2b75bf019930ae76393e0d5028a1a5f5158d77e6
Ashim Ranabhat <ranabhatashim@gmail.com>
2020-07-04 23:32:36 +1000

    refactor: Move code from services.js to server.js and delete services.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path')
-const services = require('./services');
 const user = require('./db/userData');
+const moment = require('moment');
+
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
 
 const app = express();
-const calendar = services.getCalendar();
+const calendar = getCalendar();
 
 app.use(express.static(path.join(__dirname,'/public')));
 
@@ -22,5 +23,19 @@ app.get('/calendar', (req, res)=>{
 app.get('/successfulDays', (req, res)=>{
     res.send(JSON.stringify(user.getsuccessfulDays()));
 })
+
+function getCalendar(year) {
+    if (!year) year = moment().get('year');
+    if (Number.isInteger(year) == false) throw ('year is not an integer');
+
+    let day = moment([year]).startOf('year');
+    let lastDay = moment([year]).endOf('year');
+    let yearlyCalender = [];
+
+    for (day; day <= lastDay; day.add(1, 'day')) {
+        yearlyCalender.push(day.clone().toISOString());
+    }
+    return yearlyCalender;
+}
 
 app.listen(process.env.PORT || 8080);
