7c81ed0b0fe10766a0ec6e7eb862a59b7da6ae36
Ashim Ranabhat <ranabhatashim@gmail.com>
2020-07-09 00:55:32 +1000

    fix: Delete unnecessary file test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment');
-
-let x = new Date();
-let y = moment(x);
-let today = moment();
-let date = moment('2020-07-07T14:00:00.000Z');
-
-console.log(x.toISOString(),today.toISOString(), date.toISOString())
